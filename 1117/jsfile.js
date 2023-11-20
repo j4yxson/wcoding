e8206926e8181de9f53445a24e0a71ce8e36bf9d
Jason <choijason25@gmail.com>
2023-11-20 12:10:08 +0900

    11/17

---
+++ 1117/jsfile.js
@@ -0,0 +1,27 @@
+console.log("hello")
+
+// Math.random is sensitive
+// Option1 manual way of getting heads or tails
+
+// const randNum = Math.round(Math.random());
+// console.log(randNum);
+
+// let userinput = prompt("guess heads or tails");
+
+// if (userinput === 'heads' && randNum === 0) {
+//     console.log("correct, heads");
+// }
+// else if (userinput === 'tails' && randNum === 1) {
+//     console.log("correct, tails");
+// }
+// else if (userinput === 'heads' && randNum === 1) {
+//     console.log("wrong, it was tails");
+// }
+// else if (userinput === 'tails' && randNum === 0) {
+//     console.log("wrong, it was heads");
+// }
+// else console.log("type in a valid input");
+
+
+
+
