e25c073ed773d1c14c35fe93d13c6e557431c8b8
Jason <choijason25@gmail.com>
2023-12-12 11:29:44 +0900

    1211

---
+++ 1211/exercise1.php
@@ -0,0 +1,88 @@
+<?php
+// $my_age = 10;
+// $name = "Marie";
+// $country = "Korea";
+// $isWoman = true;
+
+// if ($my_age > 3) {
+//     echo "You are really young... just only " . $my_age . " years old \n";
+// } else {
+//     echo 'it\'s okay you can continue on this web page' . $name;
+// }
+
+// if ($name == "Marie" && $country == "Korea") {
+//     echo "Welcome to " . $country . ' ' . $name;
+// };
+// if (!$isWoman || $name !== "Marie") {
+//     echo "go away";
+// };
+
+
+// $pollution_level; // Replace this with the actual pollution level
+// if else statement
+// if ($pollution_level < 50) {
+//     echo "No pollution";
+// } elseif ($pollution_level >= 50 && $pollution_level < 90) {
+//     echo "Little pollution";
+// } elseif ($pollution_level >= 90 && $pollution_level < 110) {
+//     echo "Little pollution => Wear a mask";
+// } elseif ($pollution_level >= 110 && $pollution_level < 180) {
+//     echo "Medium pollution => Wear a mask";
+// } elseif ($pollution_level >= 180 && $pollution_level < 210) {
+//     echo "High pollution => Wear a mask and no activities outside";
+// } elseif ($pollution_level >= 210) {
+//     echo "Very High pollution => Stay at home!";
+// } else {
+//     echo "No data entries, Do what you want!";
+// }
+
+
+//switches
+
+// $pollution_level = 0;
+
+// switch (true) {
+//     case ($pollution_level < 50):
+//         echo "No pollution";
+//         break;
+//     case ($pollution_level >= 50 && $pollution_level < 90):
+//         echo "Little pollution";
+//         break;
+//     case ($pollution_level >= 90 && $pollution_level < 110):
+//         echo "Little pollution => Wear a mask";
+//         break;
+//     case ($pollution_level >= 110 && $pollution_level < 180):
+//         echo "Medium pollution => Wear a mask";
+//         break;
+//     case ($pollution_level >= 180 && $pollution_level < 210):
+//         echo "High pollution => Wear a mask and no activities outside";
+//         break;
+//     case ($pollution_level >= 210):
+//         echo "Very High pollution => Stay at home!";
+//         break;
+//     default:
+//         echo "No data entries, Do what you want!";
+// }
+
+//if/else => ternary
+// $password = "banana";
+// if ($password == "banana") {
+//     echo "you have the good password " . $password . " you can enter";
+// } else {
+//     echo "Wrong password, you can't enter";
+// }
+
+// $password = ($password == "banana") ? true : false;
+
+
+// ternary => if/else;
+// $isEmployed = false;
+// if ($status === $isEmployed) {
+//     echo "no job";
+// } else {
+//     echo "you have a job";
+// }
+
+
+// $isEmployed = true;
+// echo $status = $isEmployed ? "you have a job" : "no job";
